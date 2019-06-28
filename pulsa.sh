d7701836feb02324120e7a47ae88e8698312cec4
bustedgans <50026266+bustedgans@users.noreply.github.com>
2019-06-28 21:30:49 +0700

    Add files via upload

---
+++ pulsa.sh
@@ -0,0 +1,193 @@
+#!/bin/sh
+#tools iseng iseng doang gan jgn dibawa serius:v
+#lumayan buat pamer wkwk
+#mau apa?Recoede? Malu Ster
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo '''\033[33;1m ________  _______   ______   ______   __    __
+/        |/       \ /      | /      \ /  \  /  |/       \  /      \
+$$$$$$$$/ $$$$$$$  |$$$$$$/ /$$$$$$  |$$  \ $$ |$$$$$$$  |/$$$$$$  \
+$$    |   $$    $$<   $$ |    /   $$< $$$$  $$ |$$ |  $$ |$$      \
+$$$$$/    $$$$$$$  |  $$ |   _$$$$$  |$$ $$ $$ |$$ |  $$ | $$$$$$  |
+$$ |      $$ |  $$ | _$$ |_ /  \__$$ |$$ |$$$$ |$$ |__$$ |/  \__$$ |
+$$ |      $$ |  $$ |/ $$   |$$    $$/ $$ | $$$ |$$    $$/ $$    $$/
+$$/       $$/   $$/ $$$$$$/  $$$$$$/  $$/   $$/ $$$$$$$/   $$$$$$/'''
+
+echo '''\033[33;1m ______             __
+ /      \           /  |
+/$$$$$$  | __    __ $$ |____    ______    ______
+$$ |  $$/ /  |  /  |$$      \  /      \  /      \
+$$ |      $$ |  $$ |$$$$$$$  |/$$$$$$  |/$$$$$$  |
+$$ |   __ $$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/
+$$ \__/  |$$ \__$$ |$$ |__$$ |$$$$$$$$/ $$ |
+$$    $$/ $$    $$ |$$    $$/ $$       |$$ |
+ $$$$$$/   $$$$$$$ |$$$$$$$/   $$$$$$$/ $$/
+          /  \__$$ |
+          $$    $$/
+           $$$$$$/'''
+
+echo '''\033[33;1m______
+ /      \
+/$$$$$$  |  ______   _____  ____   __    __
+$$ |__$$ | /      \ /     \/    \ /  |  /  |
+$$    $$ |/$$$$$$  |$$$$$$ $$$$  |$$ |  $$ |
+$$$$$$$$ |$$ |  $$/ $$ | $$ | $$ |$$ |  $$ |
+$$ |  $$ |$$ |      $$ | $$ | $$ |$$ \__$$ |
+$$ |  $$ |$$ |      $$ | $$ | $$ |$$    $$ |
+$$/   $$/ $$/       $$/  $$/  $$/  $$$$$$$ |
+                                  /  \__$$ |
+                                   $$$$$$/'''
+
+
+echo " "
+echo "\033[31;1m=========================================================================="
+echo "\033[31;1m+                          HACK PULSA                                    +"
+sleep 1
+echo "\033[31;1m+                  ^__^Coded By : InsanGans                              +"
+sleep 1
+echo "\033[31;1m+                  <*_*>Team : FRI3NDS CYBER ARMY                        +"
+sleep 1
+echo "\033[31;1m+                  Contact Me : 081212683289                             +"
+sleep 1
+echo "\033[31;1m=========================================================================="
+sleep 3
+
+echo " "
+echo "\033[33;1m"
+echo "[1] Telkomsel"
+sleep 1
+echo "[2] Axis"
+sleep 1
+echo "[3] Tri"
+sleep 1
+echo "[4] Indosat"
+sleep 1
+echo "[5] Smartfen"
+sleep 1
+echo "[6] Nyolong Pulsa Orang"
+sleep 1
+echo "[99] Exit"
+read -p "Pilih Nomor Nya Tod : " ins;
+
+echo " "
+if [ $ins = 1 ] || [ $ins = 01 ]
+then
+clear
+echo "\033[34;1mMasukkan Nomor Anda : "
+read anda
+sleep 2
+echo "\033[34;1mJumlah Pulsa : "
+read pulsa
+sleep 3
+echo " "
+echo "Tunggu ya Syg....."
+sleep 10
+echo "$anda Nomor Ini Berhasil Mendapatkan $pulsa"
+sleep 1
+fi
+
+echo " "
+if [ $ins = 2 ] || [ $ins = 02 ]
+then
+clear
+echo "\033[34;1mMasukkan Nomor Anda : "
+read anda
+sleep 2
+echo "\033[34;1mJumlah Pulsa : "
+read pulsa
+sleep 3
+echo " "
+echo "Tunggu ya Syg....."
+sleep 10
+echo "$anda Nomor Ini Berhasil Mendapatkan $pulsa"
+sleep 1
+fi
+
+echo " "
+if [ $ins = 3 ] || [ $ins = 03 ]
+then
+clear
+echo "\033[34;1mMasukkan Nomor Anda : "
+read anda
+sleep 2
+echo "\033[34;1mJumlah Pulsa : "
+read pulsa
+sleep 3
+echo " "
+echo "Tunggu ya Syg....."
+sleep 10
+echo "$anda Nomor Ini Berhasil Mendapatkan $pulsa"
+sleep 1
+fi
+
+echo " "
+if [ $ins = 4 ] || [ $ins = 04 ]
+then
+clear
+echo "\033[34;1mMasukkan Nomor Anda : "
+read anda
+sleep 2
+echo "\033[34;1mJumlah Pulsa : "
+read pulsa
+sleep 3
+echo " "
+echo "Tunggu ya Syg....."
+sleep 10
+echo "$anda Nomor Ini Berhasil Mendapatkan $pulsa"
+sleep 1
+fi
+
+echo " "
+if [ $ins = 5 ] || [ $ins = 05 ]
+then
+clear
+echo "\033[34;1mMasukkan Nomor Anda : "
+read anda
+sleep 2
+echo "\033[34;1mJumlah Pulsa : "
+read pulsa
+sleep 3
+echo " "
+echo "Tunggu ya Syg....."
+sleep 10
+echo "$anda Nomor Ini Berhasil Mendapatkan $pulsa"
+sleep 1
+fi
+
+echo " "
+if [ $ins = 6 ] || [ $ins = 06 ]
+then
+clear
+echo "\033[34;1mMasukkan Nomor Anda : "
+read anda
+sleep 2
+echo "\033[34;1mMasukkan Nomor Korban"
+read korban
+sleep 3
+echo "\033[34;1mJumlah Pulsa : "
+read pulsa
+sleep 3
+echo " "
+echo "Tunggu ya Syg....."
+sleep 10
+echo "$korban Nomor Ini berhasil di colong pulsanya $pulsa"
+sleep 1
+fi
+
+echo " "
+if [ $ins = 99 ] || [ $ins = 99 ]
+then
+clear
+echo "\033[34;1mSee You Next Time Sayang:)"
+sleep 1
+echo "\033[34;1mTerima Kasih Sudah Pakai Tools Saya"
+sleep 1
+fi
